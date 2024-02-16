0266cdfc809966e0710ed61a512ef618d9291e7c
Adrianxx1 <triaxis12q@gmail.com>
2024-02-16 13:33:33 -0600

    Creatingba ThemeData for the app

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -6,9 +6,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 155, 188, 201),
+        appBarTheme:const AppBarTheme(color: Color.fromARGB(255, 32, 129, 113)),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            fontFamily: 'sans-serif',
+            fontSize: 26.0,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 107, 26, 124),
+          ),
+          bodySmall: TextStyle(
+              fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+            color: Color.fromARGB(255, 148, 7, 7),
+          ),
+
+        ),
+      ),
+
 
     );
   }
